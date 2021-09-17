66e6839a06891bb1b94971e877f853af401edfe2
potatosticks <89884265+potatosticks@users.noreply.github.com>
2021-09-17 11:09:12 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ const app = Vue.createApp({
           q1: '本棚',
         },
         stage2: {
-          q1: 'いいい',
+          q1: '11',
           // q2: 'えええ',
           // q3: 'おおお'
         },
